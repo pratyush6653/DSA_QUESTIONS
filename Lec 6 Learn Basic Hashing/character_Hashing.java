fbcc405fb9a5a28f773992f47672505e2037c998
Pratyush6653 <Pratyush0231@gmail.com>
2025-02-09 19:15:16 +0530

    Character Hashing concept

---
+++ Lec 6 Learn Basic Hashing/character_Hashing.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class character_Hashing {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the string");
+        String s=sc.nextLine();
+        int hash[]=new int[26];
+
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            hash[ch-'a']++;
+        }
+        System.out.println("Enter the number of test case");
+        int q=sc.nextInt();
+        while(q-->0)
+        {
+            System.out.println("Enter the character ");
+             char ch=sc.next().charAt(0);
+             System.out.println("Frequency-->"+hash[ch-'a']);
+        }
+        
+    }
+}
